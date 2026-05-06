a88955fd0c937512f5ab27d623d5af0ce175bc10
Vinícius Souza <mviniciusy@gmail.com>
2026-05-06 05:15:39 -0300

    refactor: alteração extractor

--- finai-backend/extractor.py
+++ finai-backend/extractor.py
@@ -11,23 +11,23 @@ EMAIL_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")
 IMAP_SERVER = "imap.gmail.com"
 
 def buscar_ultimos_emails_banco():
-    print("🔄 Conectando ao IMAP...")
+    print("Conectando ao IMAP...")
     
     try:
         mail = imaplib.IMAP4_SSL(IMAP_SERVER, 993)
         mail.login(EMAIL_ACCOUNT, EMAIL_PASSWORD)
         mail.select("INBOX")
         
-        print("🔍 Buscando e-mails do Nubank...")
+        print("Buscando e-mails do Nubank...")
         status, mensagens = mail.search(None, '(FROM "nubank.com.br")')
         
         if status == "OK":
             ids_emails = mensagens[0].split()
-            print(f"🏦 Encontrados {len(ids_emails)} e-mails do Nubank.")
+            print(f"Encontrados {len(ids_emails)} e-mails do Nubank.")
             
             if ids_emails:
                 ultimo_id = ids_emails[-1]
-                print(f"📥 Baixando o e-mail mais recente (ID: {ultimo_id.decode()})...")
+                print(f"Baixando o e-mail mais recente (ID: {ultimo_id.decode()})...")
                 
                 status_fetch, dados_email = mail.fetch(ultimo_id, "(RFC822)")
                 
@@ -39,13 +39,13 @@ def buscar_ultimos_emails_banco():
                         if isinstance(assunto, bytes):
                             assunto = assunto.decode(encoding if encoding else "utf-8")
                             
-                        print(f"\n✨ ASSUNTO DO E-MAIL: {assunto}")
-                        print(f"📅 DATA: {msg.get('Date')}")
+                        print(f"\nASSUNTO DO E-MAIL: {assunto}")
+                        print(f"DATA: {msg.get('Date')}")
         
         mail.logout()
 
     except Exception as e:
-        print(f"❌ Erro: {e}")
+        print(f"Erro: {e}")
 
 if __name__ == "__main__":
     buscar_ultimos_emails_banco()
